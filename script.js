bbce069af5fdc9d4e96a5f0019a44dd9afe1973b
Ralston Hough <ralstonhough@gmail.com>
2022-09-26 19:30:28 -0400

    Made man starting size bigger

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let man;
 let clock;
 let clockScroll = 0;
-let manScroll = 4900;
+let manScroll = 5000;
 function preload() {
   man = loadImage('falling-man.png');
   clock = loadImage('wall-clock.png');
