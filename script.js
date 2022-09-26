8e6cbd3718c10800ba94d683c1256bcbb11bc345
Ralston Hough <ralstonhough@gmail.com>
2022-09-26 17:59:58 -0400

    Added clocks and rotation

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-let img;
+let man;
+let clock;
 function preload() {
-  img = loadImage('falling-man.png');
+  man = loadImage('falling-man.png');
+  clock = loadImage('wall-clock.png');
 };
 
 function setup() {
     createCanvas(700,700);
     background("black");
     translate(width / 2, height / 2);
-    noStroke();
-    circle(0,0,10);
+    image(clock,0,0,10,10);
 };
 
 function draw() {
     translate(width / 2, height / 2);
-    circleOfCircles(15, 0, 10);
-    circleOfCircles(35, 5, 20);
-    circleOfCircles(65, 10, 30);
-    circleOfCircles(115, 15, 60);
-    circleOfCircles(210, 20, 120);
-    circleOfCircles(400, 25, 240);
+
+    push();
+    background("black");
+    rotate(-millis() / 3000.0);
+    circleOfClocks(15, 0, 10, 10);
+    circleOfClocks(35, 5, 20, 20);
+    circleOfClocks(65, 10, 30, 30);
+    circleOfClocks(115, 15, 60, 60);
+    circleOfClocks(210, 20, 120, 120);
+    circleOfClocks(400, 25, 240, 240);
+    pop();
 
     imageMode(CENTER);
-    image(img, 0, 0, 100, 100);
+    image(man, 0, 0, 100, 100);
 };
 
-function circleOfCircles(r, angle, size){
+function circleOfClocks(r, angle, width, height){
   for(let i=0; i<7; i++){
     let x = r * sin(angle);
     let y = r * cos(angle);
-    circle(x, y, size);
+    imageMode(CENTER);
+    image(clock, x, y, width, height);
     angle = angle + TWO_PI/6;
   };
 };
